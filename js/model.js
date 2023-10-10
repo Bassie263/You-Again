9346876104eb11f3c4a1ab1b3354bfc10e099426
Bassie263 <bas.bulckaen@student.howest.be>
2023-10-10 16:56:44 +0200

    left for today

--- js/model.js
+++ js/model.js
@@ -1,73 +1,81 @@
-let featureExtractor;
-let regressor;
-let $addDataBtn, $trainBtn, $classifyBtn;
-let numSamples = 0;
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let cv;
 
-const init = async () => {
-    console.log('ml5 '+ ml5.version);
-
-
-    const $video = document.querySelector('.video');
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    $video.srcObject = stream;
-    $video.play();
-
-    const options = {
-        debug : true
-    }
-
-    featureExtractor = await ml5.featureExtractor('MobileNet', options, modelLoaded);
-    regressor = await featureExtractor.regression($video, videoReady);
-
-
-    const imageClassifier = ml5.imageClassifier('MobileNet', () => {
-        console.log('Model Ready');
-
-    });
-
-    $addDataBtn = document.querySelector('.addData');
-    $addDataBtn.addEventListener('click', addDataHandler);
-    $addDataBtn.disabled = true;
-
-    $trainBtn = document.querySelector('.trainModel');
-    $trainBtn.addEventListener('click', startTraining);
-    $trainBtn.disabled = true;
-
-
-
-    
-};
-
-
-const modelLoaded = () => {
-    document.querySelector('.status').textContent = 'Base model Mobilenet loaded';
-    $addDataBtn.disabled = false;
-  
-};
-
-const addDataHandler = () => {
-    console.log('data added');document.querySelector('.status').textContent = `${++numSamples} samples added.`;
-    if (numSamples > 0) {
-      $trainBtn.disabled = false;
-    }
-};
-
-const startTraining = () => {
-    regressor.train(trainHandler)
+function onOpenCvReady() {
+    cv = cv || window.cv;
+    startColorDetection();
 }
 
-trainHandler = (lossValue) => {
-    if (lossValue) {
-        document.querySelector('.status').textContent = `Loss: ${lossValue}`;
-      } else {
-        document.querySelector('.status').textContent = `Done training!`;
-        $classifyBtn.disabled = false;
-        $saveBtn.disabled = false;
-      }
+function setup() {
+    // Initialiseren van de webcam
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        video.srcObject = stream;
+        video.play();
+    }).catch((error) => {
+        console.error('Webcam toegang geweigerd: ', error);
+    });
+
+    // Wachten tot de video is geladen
+    video.onloadedmetadata = () => {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        startColorDetection();
+    };
+}
+
+function startColorDetection() {
+    if (!cv) {
+        setTimeout(startColorDetection, 50);
+        return;
     }
 
-const videoReady = () => {
-    document.querySelector('.status').textContent = 'Video loaded';
-  }
+    const cap = new cv.VideoCapture(video);
+    const frame = new cv.Mat(video.videoHeight, video.videoWidth, cv.CV_8UC4);
 
-init();
+    const contours = new cv.MatVector();
+    const hierarchy = new cv.Mat();
+
+    const low = new cv.Scalar(0, 0, 0, 0);
+    const high = new cv.Scalar(255, 255, 255, 255);
+
+    const maxArea = -1; // Om alle vierkanten te detecteren
+
+    function processVideo() {
+        cap.read(frame);
+        cv.cvtColor(frame, frame, cv.COLOR_RGBA2RGB);
+        cv.cvtColor(frame, frame, cv.COLOR_RGB2HSV);
+        cv.inRange(frame, low, high, frame);
+
+        cv.findContours(frame, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+
+        for (let i = 0; i < contours.size(); i++) {
+            const contour = contours.get(i);
+            const area = cv.contourArea(contour, false);
+            if (area > maxArea) {
+                const rect = cv.boundingRect(contour);
+                cv.rectangle(frame, rect, new cv.Scalar(255, 0, 0, 255), 2);
+
+                // Voer hier je kleuranalyse uit op het vierkant, bijvoorbeeld door het gemiddelde te berekenen
+                const squareROI = frame.roi(rect);
+                const averageColor = cv.mean(squareROI);
+                console.log('Gemiddelde kleur:', averageColor);
+
+                squareROI.delete();
+            }
+            contour.delete();
+        }
+
+        cv.imshow(canvas, frame);
+        frame.delete();
+        contours.delete();
+        hierarchy.delete();
+        requestAnimationFrame(processVideo);
+    }
+
+    processVideo();
+}
+
+// Start de setup wanneer het venster geladen is
+window.addEventListener('load', setup);
