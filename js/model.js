eed6427f9d8f94997ba4049925efcaf41b41b8e6
Bassie263 <bas.bulckaen@student.howest.be>
2023-10-12 16:18:23 +0200

    copied code of Demo 15 and and tried to tweak as much as possible

--- js/model.js
+++ js/model.js
@@ -1,81 +1,61 @@
-const video = document.getElementById('video');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-let cv;
+let knnClassifier;
+let inputs;
+let featureExtractor;
 
-function onOpenCvReady() {
-    cv = cv || window.cv;
-    startColorDetection();
+const init = async () => {
+  console.log('ml5 version:', ml5.version);
+  //document.body.append(document.createTextNode(ml5.version));
+
+  knnClassifier = ml5.KNNClassifier();
+
+
+  const $video = document.querySelector('.video');
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+  $video.srcObject = stream;
+  $video.play();
+
+  const color = await ml5.featureExtractor('MobileNet', modelReady);
+  const features = featureExtractor
+  const $label = document.querySelector('.label');
+
+
+  const $addDataButton = document.querySelector('.addData');
+  $addDataButton.addEventListener('click', addDataHandler);
+
+  const $classifyButton = document.querySelector('.classify');
+  $classifyButton.addEventListener('click', classifyHandler);
 }
 
-function setup() {
-    // Initialiseren van de webcam
-    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-        video.srcObject = stream;
-        video.play();
-    }).catch((error) => {
-        console.error('Webcam toegang geweigerd: ', error);
-    });
 
-    // Wachten tot de video is geladen
-    video.onloadedmetadata = () => {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        startColorDetection();
-    };
+const classifyHandler = () => {
+  knnClassifier.classify(inputs, classifyResultHandler);
 }
 
-function startColorDetection() {
-    if (!cv) {
-        setTimeout(startColorDetection, 50);
-        return;
-    }
 
-    const cap = new cv.VideoCapture(video);
-    const frame = new cv.Mat(video.videoHeight, video.videoWidth, cv.CV_8UC4);
-
-    const contours = new cv.MatVector();
-    const hierarchy = new cv.Mat();
-
-    const low = new cv.Scalar(0, 0, 0, 0);
-    const high = new cv.Scalar(255, 255, 255, 255);
-
-    const maxArea = -1; // Om alle vierkanten te detecteren
-
-    function processVideo() {
-        cap.read(frame);
-        cv.cvtColor(frame, frame, cv.COLOR_RGBA2RGB);
-        cv.cvtColor(frame, frame, cv.COLOR_RGB2HSV);
-        cv.inRange(frame, low, high, frame);
-
-        cv.findContours(frame, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-
-        for (let i = 0; i < contours.size(); i++) {
-            const contour = contours.get(i);
-            const area = cv.contourArea(contour, false);
-            if (area > maxArea) {
-                const rect = cv.boundingRect(contour);
-                cv.rectangle(frame, rect, new cv.Scalar(255, 0, 0, 255), 2);
-
-                // Voer hier je kleuranalyse uit op het vierkant, bijvoorbeeld door het gemiddelde te berekenen
-                const squareROI = frame.roi(rect);
-                const averageColor = cv.mean(squareROI);
-                console.log('Gemiddelde kleur:', averageColor);
-
-                squareROI.delete();
-            }
-            contour.delete();
-        }
-
-        cv.imshow(canvas, frame);
-        frame.delete();
-        contours.delete();
-        hierarchy.delete();
-        requestAnimationFrame(processVideo);
-    }
-
-    processVideo();
+const classifyResultHandler = (error, result) => {
+  if (error) return console.error(error);
+  console.log(result);
+  document.querySelector('.status').textContent = result.label;
 }
 
-// Start de setup wanneer het venster geladen is
-window.addEventListener('load', setup);
+
+const addDataHandler = () => {
+  const $label = document.querySelector('.label');
+  const output = $label.value;
+  knnClassifier.addExample(inputs, output);
+}
+
+
+const resultHandler = (results) => {
+  if (!results.length) return;
+
+  inputs = results[0].landmarks.flat();
+  console.log(inputs);
+}
+
+
+const modelReady = () => {
+  console.log("model loaded");
+}
+
+init();
