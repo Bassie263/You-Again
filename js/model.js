cbb4149f4c18a95ea1925f58902295b212b1d1a8
Bassie263 <bas.bulckaen@student.howest.be>
2023-10-13 13:42:44 +0200

    almost ready model trainer

--- js/model.js
+++ js/model.js
@@ -1,61 +1,74 @@
 let knnClassifier;
 let inputs;
 let featureExtractor;
+let video;
 
 const init = async () => {
   console.log('ml5 version:', ml5.version);
-  //document.body.append(document.createTextNode(ml5.version));
 
   knnClassifier = ml5.KNNClassifier();
+  video = document.querySelector('.video');
 
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    video.srcObject = stream;
+    video.play();
+  } catch (error) {
+    console.error('Fout bij het verkrijgen van toegang tot de webcam:', error);
+  }
 
-  const $video = document.querySelector('.video');
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-  $video.srcObject = stream;
-  $video.play();
-
-  const color = await ml5.featureExtractor('MobileNet', modelReady);
-  const features = featureExtractor
-  const $label = document.querySelector('.label');
-
+  featureExtractor = await ml5.featureExtractor('MobileNet', modelReady);
+  featureExtractor.on = ('predict', resultHandler);
 
   const $addDataButton = document.querySelector('.addData');
   $addDataButton.addEventListener('click', addDataHandler);
 
   const $classifyButton = document.querySelector('.classify');
   $classifyButton.addEventListener('click', classifyHandler);
-}
 
+  const $saveButton = document.querySelector('.save');
+  $saveButton.addEventListener('click', modelSaveHandler);
+}
 
 const classifyHandler = () => {
-  knnClassifier.classify(inputs, classifyResultHandler);
+    knnClassifier.classify(inputs, classifyResultHandler);
 }
-
 
 const classifyResultHandler = (error, result) => {
-  if (error) return console.error(error);
-  console.log(result);
-  document.querySelector('.status').textContent = result.label;
+  if (error) {
+    console.error('Fout bij classificeren:', error);
+  } else {
+    console.log('Classificatie resultaat:', result);
+    document.querySelector('.status').textContent = result.label;
+  }
 }
-
 
 const addDataHandler = () => {
-  const $label = document.querySelector('.label');
-  const output = $label.value;
-  knnClassifier.addExample(inputs, output);
-}
+  const label = document.querySelector('.label');
+  const output = label.value;
+  knnClassifier.addExample = (inputs, output);
+    console.log('Gegevens toegevoegd:', inputs, output);
 
+}
 
 const resultHandler = (results) => {
-  if (!results.length) return;
+    if (!results.length) return;
 
-  inputs = results[0].landmarks.flat();
-  console.log(inputs);
+    inputs = results[0].landmarks.flat();
 }
 
-
 const modelReady = () => {
-  console.log("model loaded");
+  console.log('Model is gereed.');
+}
+
+const modelSaveHandler = () => {
+  if (knnClassifier) {
+    knnClassifier.save('model.json', () => {
+      console.log('Model opgeslagen.');
+    });
+  } else {
+    console.error('KNNClassifier is niet gedefinieerd.');
+  }
 }
 
 init();
